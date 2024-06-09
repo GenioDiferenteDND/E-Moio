90b7b66121ac1a767b076182fc0b879fe5318988
GenioDiferenteDND <130307809+GenioDiferenteDND@users.noreply.github.com>
2024-06-09 18:57:26 +0200

    Update FeaturedProducts.jsx

--- themes/evershop/pages/homepage/FeaturedProducts.jsx
+++ themes/evershop/pages/homepage/FeaturedProducts.jsx
@@ -3,60 +3,54 @@ import { _ } from "@evershop/evershop/src/lib/locale/translate";
 import PropTypes from "prop-types";
 import React from "react";
 
-export default function FeaturedProducts({ collections }) {
-  if (!collections || collections.length === 0) {
+export default function FeaturedProducts({ collection }) {
+  if (!collection) {
     return null;
   }
   return (
     <div className="pt-3">
       <div className="page-width">
-        {collections.map((collection) => (
-          <div key={collection.collectionId}>
-            <h2 className="mt-3 mb-3 text-center uppercase tracking-widest">
-              {collection.name}
-            </h2>
-            <ProductList products={collection.products.items} countPerRow={3} />
-          </div>
-        ))}
+        <h2 className="mt-3 mb-3 text-center uppercase  tracking-widest">
+          {collection.name}
+        </h2>
+        <ProductList products={collection.products.items} countPerRow={3} />
       </div>
     </div>
   );
 }
 
 FeaturedProducts.propTypes = {
-  collections: PropTypes.arrayOf(
-    PropTypes.shape({
-      collectionId: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      products: PropTypes.shape({
-        items: PropTypes.arrayOf(
-          PropTypes.shape({
-            productId: PropTypes.number.isRequired,
-            name: PropTypes.string.isRequired,
-            price: PropTypes.shape({
-              regular: PropTypes.shape({
-                value: PropTypes.number.isRequired,
-                text: PropTypes.string.isRequired,
-              }).isRequired,
-              special: PropTypes.shape({
-                value: PropTypes.number.isRequired,
-                text: PropTypes.string.isRequired,
-              }).isRequired,
+  collection: PropTypes.shape({
+    collectionId: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    products: PropTypes.shape({
+      items: PropTypes.arrayOf(
+        PropTypes.shape({
+          productId: PropTypes.number.isRequired,
+          name: PropTypes.string.isRequired,
+          price: PropTypes.shape({
+            regular: PropTypes.shape({
+              value: PropTypes.number.isRequired,
+              text: PropTypes.string.isRequired,
             }).isRequired,
-            image: PropTypes.shape({
-              alt: PropTypes.string.isRequired,
-              url: PropTypes.string.isRequired,
+            special: PropTypes.shape({
+              value: PropTypes.number.isRequired,
+              text: PropTypes.string.isRequired,
             }).isRequired,
+          }).isRequired,
+          image: PropTypes.shape({
+            alt: PropTypes.string.isRequired,
             url: PropTypes.string.isRequired,
-          })
-        ).isRequired,
-      }).isRequired,
-    })
-  ).isRequired,
+          }).isRequired,
+          url: PropTypes.string.isRequired,
+        })
+      ).isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 FeaturedProducts.defaultProps = {
-  collections: [],
+  featuredProducts: [],
 };
 
 export const layout = {
@@ -66,30 +60,28 @@ export const layout = {
 
 export const query = `
   query query {
-    collections {
-      items {
-        collectionId
-        name
-        products (filters: [{key: "limit", operation: eq, value: "6"}]) {
-          items {
-            productId
-            name
-            price {
-              regular {
-                value
-                text
-              }
-              special {
-                value
-                text
-              }
+    collection (code: "homepage") {
+      collectionId
+      name
+      products (filters: [{key: "limit", operation: eq, value: "6"}]) {
+        items {
+          productId
+          name
+          price {
+            regular {
+              value
+              text
             }
-            image {
-              alt
-              url: listing
+            special {
+              value
+              text
             }
-            url
+            }
+          image {
+            alt
+            url: listing
           }
+          url
         }
       }
     }
